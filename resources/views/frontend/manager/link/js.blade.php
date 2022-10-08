e22f6ce07d7cff22ac35e1bdd16585760382afc3
tienbm12 <tienbm1208@gmail.com>
2022-10-08 20:35:53 +0700

    add laravel

---
+++ resources/views/frontend/manager/link/js.blade.php
@@ -0,0 +1,45 @@
+ <!-- ==== jQuery Library ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery-3.2.1.min.js"></script>
+
+ <!-- ==== Bootstrap Framework ==== -->
+ <script src="{{ url('/frontend') }}/js/bootstrap.min.js"></script>
+
+ <!-- ==== StickyJS Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.sticky.min.js"></script>
+
+ <!-- ==== HoverIntent Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.hoverIntent.min.js"></script>
+
+ <!-- ==== Marquee Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.marquee.min.js"></script>
+
+ <!-- ==== Validation Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.validate.min.js"></script>
+
+ <!-- ==== Isotope Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/isotope.min.js"></script>
+
+ <!-- ==== Resize Sensor Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/resizesensor.min.js"></script>
+
+ <!-- ==== Sticky Sidebar Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/theia-sticky-sidebar.min.js"></script>
+
+ <!-- ==== Zoom Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.zoom.min.js"></script>
+
+ <!-- ==== Bar Rating Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.barrating.min.js"></script>
+
+ <!-- ==== Countdown Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/jquery.countdown.min.js"></script>
+
+ <!-- ==== RetinaJS Plugin ==== -->
+ <script src="{{ url('/frontend') }}/js/retina.min.js"></script>
+
+ <!-- ==== Google Map API ==== -->
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBK9f7sXWmqQ1E-ufRXV3VpXOn_ifKsDuc"></script>
+
+ <!-- ==== Main JavaScript ==== -->
+ <script src="{{ url('/frontend') }}/js/main.js"></script>
+ @yield('jsfrontend')
